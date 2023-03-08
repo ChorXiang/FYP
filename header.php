35c3d3bdc4bc14d4d4d58004fece6a1bfd8c53d8
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-09 00:33:23 +0800

    header

--- header.php
+++ header.php
@@ -9,13 +9,13 @@
     <style>
         .upper
         {
-            padding:20px ;
+            padding: 16px 30px;
             font-size: 27px;
             background:blue;
         }
         .center
         {
-            margin: 0 30px;
+            margin: 0 50px;
             color: white;
         }
         .right
@@ -33,6 +33,7 @@
         .middle
         {
             text-align: center;
+
         }
         ul
         {
@@ -40,50 +41,46 @@
             margin: 0;
             overflow: hidden;
         }
-        li
+        li a 
         {
-            float: right;
+            display: inline-block;
+            color: white;
+            text-align: center;
+            text-decoration: none;
         }
-
-
-        li a, .dropbtn {
-  display: inline-block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover, .dropdown:hover .dropbtn {
-  background-color: red;
-}
-
-li.dropdown {
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown-content a:hover {background-color: #f1f1f1;}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
+        .dropdown
+        {
+            margin: 0 50px;
+        }
+        li.dropdown 
+        {
+            display: inline-block;
+        }
+        .dropdown-content 
+        {
+            display: none;
+            position: absolute;
+            background-color: white;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+        .dropdown-content a 
+        {
+            color: black;
+            padding: 16px 30px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+        .dropdown-content a:hover 
+        {
+            color: blue;
+        }
+        .dropdown:hover .dropdown-content 
+        {
+            display: block;
+        }
     </style>
 <title></title>
 </head>
@@ -94,43 +91,45 @@ li.dropdown {
     </div>
     
     <ul >
-        <li class="center"><a href="#"  class="center" ><i class="fa fa-shopping-cart">Shopping cart</i></a></li>
-        <li class="dropdown"><a href="#"class="javascript:void(0)" class="dropbtn">Login</a></li>
+        <li  style="float: right;"><a href="#"  class="center" ><i class="fa fa-shopping-cart">Shopping cart</i></a></li>
+        <li class="dropdown" style="float: right;"><a href="#"class="javascript:void(0)" class="dropbtn">Login</a>
             <div class="dropdown-content">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
+                <a href="#">My Account</a>
+                <a href="#">My Wish List</a>
+                <a href="#">Create An Account</a>
+                <a href="#">Login</a>
             </div>
+            </li>
     </ul>
 
     <hr>
         <div class="middle">
-        <a href="#" class="center">Brands</a>
-        <a href="#" class="center">Category</a>
-        <a href="#" class="center">Sales</a>
+        <ul >
+            <li class="dropdown"><a href="#"class="javascript:void(0)" class="dropbtn">Brands</a>
+                <div class="dropdown-content">
+                    <a href="#">Nike</a>
+                    <a href="#">Puma</a>
+                    <a href="#">Adidas</a>
+                    <a href="#">Vans</a>
+                </div>
+            </li>
+
+            <li class="dropdown"><a href="#"class="javascript:void(0)" class="dropbtn">Category</a>
+                <div class="dropdown-content">
+                    <a href="#">Man</a>
+                    <a href="#">Woman</a>
+                    <a href="#">Kid</a>
+                </div>
+            </li>
+
+            <li class="dropdown"><a href="#">Sales</a>
+            </li>
+        </ul>
+
         </div>
 
-</div>
 
-
-
-
-    <!-- <div class="upper">
-
-        <a href="#" class="center" ><img src="img/logo.png" alt="Shop Logo"></a>
-        <span class="right">
-
-            Login
-
-            <a href="#" class="center"><i class="fa fa-shopping-cart">Shopping cart</i></a>
-        </span>
-        <hr>
-        <div class="middle">
-        <a href="#" class="center">Brands</a>
-        <a href="#" class="center">Category</a>
-        <a href="#" class="center">Sales</a>
         </div>
-    </div>     -->
 
     
 </body>
