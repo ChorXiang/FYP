2e518178bf1798dec5340e53302b033a94107f98
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-20 15:46:11 +0800

    order

--- order.php
+++ order.php
@@ -41,7 +41,54 @@
   </style>
 </head>
 <body>
+<div class="middle">
+
+<fieldset>
+    <?php
+    $sql = "SELECT * FROM orders";
+    $result = mysqli_query($conn,$sql);
+    $id = $_GET['email'];
+    $host = "SELECT * FROM `user` where Email = '$id'";
+    $query = mysqli_query($conn,$host);
+    $host_image = mysqli_fetch_assoc($query);
+    ?>
+  <h1><i class="fa fa-shopping-cart" style="font-size:50px"></i><b style="font-size: 50px;"> Order</b></h1>
+  <table border="0px">
+    <tr>
+      <td>Food Name </td>
+      <td>Food Quantity</td>
+      <td>Food Price per Item</td>
+    </tr>
+    <?php
+
+    while($row = mysqli_fetch_array($result))
+    {
+        ?>
+    <tr>
+      <td><?php echo $row["foodname"]; ?></td>
+      <td><?php echo $row["quantity"];	?></td>
+      <td>RM<?php echo $row["price"];?></td>
+      <td><a href="deleteorder.php?order_ID=<?php echo $row['order_ID']; ?>&&email=<?php echo $id?>"><i class="fa fa-close" style="font-size:36px;color:#dc3545;"></i></a>
+      </td>
+    </tr>
+        <?php
     
+    }
+
+    ?>
+        
+  </table>
+  <button style="background-color: gray;">1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>4</button>
+  <button>5</button>
+  <button>Next Page</button><span class="left"><a href="Homepage.php?email=<?php echo $id?>" alt="insert"><i class="fa fa-plus-square"></i> <input type="button" value="Add-On"></span></p>
+  <a href="Homepage.php?email=<?php echo $id?>">Return Home page
+  <div class="left"><a href="payment.php?email=<?php echo $id?>" alt="payment"><i class="fa fa-plus-square"></i> <input type="button" value="Checkout"></div>
+
+</fieldset>
+</div>
 </body>
 </html>
 
