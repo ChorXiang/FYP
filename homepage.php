dcb7e9e9ba91fb3b162cd2e1a975786336252f52
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-20 13:54:33 +0800

    list png

--- homepage.php
+++ homepage.php
@@ -33,19 +33,21 @@
 </head>
 <body>
 <div class="w3-content w3-display-container" style="max-width:10000px">
-  <img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/t/r/treziodpt-banner.jpg" style="width:100%">
-  <img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/f/o/forum84low-banner.jpg" style="width:100%">
-  <img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/h/u/humara-banner.jpg" style="width:100%">
+<a href="#"><img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/t/r/treziodpt-banner.jpg" style="width:100%"></a>
+<a href="#"><img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/f/o/forum84low-banner.jpg" style="width:100%"></a>
+<a href="#"><img class="mySlides" src="https://atmos-kl.com/pub/media/mageplaza/bannerslider/banner/image/h/u/humara-banner.jpg" style="width:100%"></a>
   <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
     <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
     <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
     <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+
   </div>
+
 </div> 
 
-
+<p><img src="image/list.png" style="width:100%"></p>
 
 <script>
 var slideIndex = 1;
