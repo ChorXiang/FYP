ea8707f673041e52c885277db7faeb643cb8ad98
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-20 15:39:50 +0800

    order

---
+++ order.php
@@ -0,0 +1,52 @@
+<?php
+    include 'header.php';
+    include 'conn.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Order Page</title>
+    <style>
+    fieldset
+    {
+        background-color: #f2f2f2;
+    }
+    .middle
+    {
+        max-width: 800px;
+        margin: auto; 
+    }
+    *
+    {
+      font-size: 20px;
+    }
+    .left
+    {
+      float: right;
+      margin-bottom: 50px;
+    }
+    td, th 
+    {
+      text-align: left;
+      padding: 30px;
+    }
+    a:hover
+    {
+      color: red;
+    }
+  </style>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+<?php
+    include 'footer.php';
+   
+?>
