2d7949889eff4b7cac71f0f8fb80e460cf079981
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-21 22:17:59 +0800

    database updatate 21/3/2023

--- fyp.sql
+++ fyp.sql
@@ -72,3 +72,175 @@ COMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
+---------------------------------------------------------------- Latest Database 21/3/2023 --------------------------------------------------------------
+
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2023 at 03:16 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fyp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `subject`, `message`) VALUES
+(1, 'testing', 'test@gmail.com', 'no', 'no'),
+(2, 'key', 'test@gmail.com', 'key', 'key');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` int(30) NOT NULL,
+  `shoesname` varchar(30) NOT NULL,
+  `price` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_ID`, `shoesname`, `price`, `quantity`, `username`) VALUES
+(35, 'dunk low panda', 489, 2, ''),
+(36, 'Air Jordan', 420, 3, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shoes`
+--
+
+CREATE TABLE `shoes` (
+  `shoe_id` int(11) NOT NULL,
+  `shoe_name` varchar(255) NOT NULL,
+  `shoe_type` varchar(50) NOT NULL,
+  `shoe_image` varchar(255) NOT NULL,
+  `shoe_size` varchar(10) NOT NULL,
+  `shoe_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shoes`
+--
+
+INSERT INTO `shoes` (`shoe_id`, `shoe_name`, `shoe_type`, `shoe_image`, `shoe_size`, `shoe_price`) VALUES
+(1, 'Nike Air Max', 'Running', 'https://example.com/images/nike-air-max.jpg', 'US 10', '129.99');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(100) NOT NULL,
+  `full_name` varchar(50) NOT NULL,
+  `contact_no` varchar(14) NOT NULL,
+  `email_address` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `userpassword` varchar(30) NOT NULL,
+  `confirm_password` varchar(30) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `full_name`, `contact_no`, `email_address`, `username`, `userpassword`, `confirm_password`, `image`, `status`) VALUES
+(1, 'abccccc', '0000000000', 'elwinwong@gmail.com', 'elwin', '123', ' 123', 'profile.jpg', ''),
+(2, 'abccccc', '0000000000', 'elwinwong@gmail.com', 'Elwinwong03', '123', ' 123', 'profile.jpg', ''),
+(4, 'abccccc', '0000000000', 'elwinwong@gmail.com', 'elwin', '12345', '12345', 'profile.jpg', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`);
+
+--
+-- Indexes for table `shoes`
+--
+ALTER TABLE `shoes`
+  ADD PRIMARY KEY (`shoe_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_ID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ wishlist.php
@@ -0,0 +1,39 @@
+<?php
+    include 'header.php';
+    include 'conn.php'; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Wishlist Page</title>
+
+    <style>
+    .box
+    {
+     background-color: #f2f2f2;
+     padding: 5px 20px 15px 20px;
+     border: 1px solid lightgrey;
+     border-radius: 15px;
+     width:50%;
+     padding: 5px;
+     margin: 20px 460px;
+    }
+    </style>
+
+</head>
+<body>
+<div class="box">
+
+
+</div>    
+</body>
+</html>
+
+<?php
+    include 'footer.php';
+   
+?>
