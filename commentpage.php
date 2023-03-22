5584ed6295eb62763973ae51c95c26f25caf4404
Murphy1211201894 <1211201894@student.mmu.edu.my>
2023-03-22 12:08:39 +0800

    comment page update

--- commentpage.php
+++ commentpage.php
@@ -1,22 +1,49 @@
 <?php
 include 'header.php'; 
 include 'conn.php'; 
-?>
 
-<?php
+// Initialize variables
+$email = "";
+$emailErr = "";
+$shipping_ratingErr = "";
+$customer_service_ratingErr = "";
+$product_quality_ratingErr = "";
 
 // Check if the user has submitted a rating
 if (isset($_POST['submit_rating'])) {
 
-  if (isset($_POST['email'])) {
-    $email = $_POST['email'];
+  // Validate email
+  if (empty($_POST["email"])) {
+    $emailErr = " * Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "*Invalid Email Format!"; 
+    }
   }
-    // Get the user's ratings for each service
+
+  // Validate shipping rating
+  if (empty($_POST["shipping_rating"])) {
+    $shipping_ratingErr = " * Rating is required";
+  }
+
+  // Validate customer service rating
+  if (empty($_POST["customer_service_rating"])) {
+    $customer_service_ratingErr = " * Rating is required";
+  }
+
+  // Validate product quality rating
+  if (empty($_POST["product_quality_rating"])) {
+    $product_quality_ratingErr = " * Rating is required";
+  }
+
+  // If all fields are valid, store the ratings in the session
+  if ($emailErr == "" && $shipping_ratingErr == "" && $customer_service_ratingErr == "" && $product_quality_ratingErr == "") {
     $shipping_rating = $_POST['shipping_rating'];
     $customer_service_rating = $_POST['customer_service_rating'];
     $product_quality_rating = $_POST['product_quality_rating'];
 
-    // Store the ratings in the session
     $_SESSION['email'] = $email;
     $_SESSION['shipping_rating'] = $shipping_rating;
     $_SESSION['customer_service_rating'] = $customer_service_rating;
@@ -24,6 +51,15 @@ if (isset($_POST['submit_rating'])) {
 
     // Display a message to the user
     echo "Thank you for rating our services!";
+  }
+}
+
+// This PHP function takes a string input and applies various sanitization techniques, such as removing whitespace and special characters, to ensure that the data is safe and secure for further processing.
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
 
 ?>
@@ -42,15 +78,17 @@ if (isset($_POST['submit_rating'])) {
 <fieldset> 
 	<h1>Online Shoe Selling Store Service Rating</h1>
 	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-    <label for="email">Email:</label>
-    <input type="text" name="email" id="email" required>
-    <br>
-		<h2>Shipping</h2>
+  <br><br>
+  Email：<input type="email" name="email" value="<?php echo $email;?>">
+  <span class="error"><?php echo $emailErr;?></span>
+  <br><br>
+		<h2>Shipping Service</h2>
 		<input type="radio" name="shipping_rating" value="1">1
 		<input type="radio" name="shipping_rating" value="2">2
 		<input type="radio" name="shipping_rating" value="3">3
 		<input type="radio" name="shipping_rating" value="4">4
 		<input type="radio" name="shipping_rating" value="5">5
+    <span class="error"><?php echo $shipping_ratingErr;?></span>
 		<br>
 
 		<h2>Customer Service</h2>
@@ -59,6 +97,7 @@ if (isset($_POST['submit_rating'])) {
 		<input type="radio" name="customer_service_rating" value="3">3
 		<input type="radio" name="customer_service_rating" value="4">4
 		<input type="radio" name="customer_service_rating" value="5">5
+    <span class="error"><?php echo $customer_service_ratingErr;?></span>
 		<br>
 
 		<h2>Product Quality</h2>
@@ -67,6 +106,7 @@ if (isset($_POST['submit_rating'])) {
 		<input type="radio" name="product_quality_rating" value="3">3
 		<input type="radio" name="product_quality_rating" value="4">4
 		<input type="radio" name="product_quality_rating" value="5">5
+    <span class="error"><?php echo $product_quality_ratingErr;?></span>
 		<br>
 
 		<input type="submit" name="submit_rating" value="Submit Rating">
