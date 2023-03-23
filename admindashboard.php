204fa93515164dd8225254dbf3871f22feebc8b6
ChorXiang <1211201763@student.mmu.edu.my>
2023-03-23 18:17:02 +0800

    admin dashboard n header ok

--- admindashboard.php
+++ admindashboard.php
@@ -1,3 +1,9 @@
+<?php
+    include 'adminheader.php';
+    include 'conn.php'; 
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +11,121 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+
+    <style>
+        body
+        {
+            background-color: lightgrey;
+        }
+        .middlee
+        {
+            max-width: 1500px;
+            margin: auto; 
+            display: flex;
+            text-align:center;
+        }
+        .box
+        {
+            background-color: #f2f2f2;
+            padding: 25px 20px 15px 20px;
+            border: 1px solid lightgrey;
+            border-radius: 15px;
+            float: left;
+            width: 55%;
+            margin: 20px 30px;
+            box-shadow: 3px 3px 5px gray;
+        }
+        .box2
+        {
+            background-color: #f2f2f2;
+            padding: 25px 20px 15px 20px;
+            border: 1px solid lightgrey;
+            border-radius: 15px;
+            float: right;
+            width: 55%;
+            margin: 20px 30px;
+            box-shadow: 3px 3px 5px gray;
+        }
+        .select
+        {
+            color:white;
+            background-color: black;
+            border-radius: 4px;
+            border: 1px solid #ddd;
+            display: block;
+            padding: 50px 15px;
+            margin:20px 20px;
+            font-size: 27px;
+        }
+        .select:hover
+        {
+            color:black;
+            background-color: white;
+        }
+        a.hover
+        {
+            color:red;
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="middlee">
+
+        <div class="box">
+
+        <a href="#" class="select">Manage Category</a>
+
+        </div>
+
+        <div class="box2">
+
+        <a href="#" class="select">Manage Product</a>
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+    <div class="middlee">
+
+        <div class="box">
+
+        <a href="#" class="select">Manage Customer</a>
+
+
+        </div>
+
+        <div class="box2">
+
+        <a href="#" class="select">Manage Staff </a>
+
+
+        </div>
+
+    </div>
+
+    <div class="middlee">
+
+        <div class="box">
+
+        <a href="#" class="select">Manage Order</a>
+
+
+        </div>
+
+        <div class="box2">
+
+        <a href="#" class="select">View n Print Report </a>
+
+
+        </div>
+
+    </div>
+
+
 </body>
 </html>
---
+++ adminheader.php
@@ -0,0 +1,119 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Header</title>
+    <style>
+        .upper
+        {
+            padding: 35px 30px;
+            font-size: 27px;
+            background:black;
+        }
+        *
+        {
+            margin:0;
+        }
+        .center
+        {
+            margin: 0 50px;
+            color: white;
+        }
+        img
+        {
+            width: 90px;
+        }
+        .right
+        {
+            float:right;
+        }
+        a
+        {
+            text-decoration:none;
+        }
+        .middle
+        {
+            text-align: center;
+        }
+        ul
+        {
+            list-style-type: none;
+            margin: 0;
+            overflow: hidden;
+        }
+        li a 
+        {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            text-decoration: none;
+        }
+        .dropdown
+        {
+            margin: 0 50px;
+        }
+        li.dropdown 
+        {
+            display: inline-block;
+        }
+        .dropdown-content 
+        {
+            display: none;
+            position: absolute;
+            background-color: white;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+        .dropdown-content a 
+        {
+            color: black;
+            padding: 16px 30px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+        .dropdown-content a:hover 
+        {
+            color: red;
+        }
+        .dropdown:hover .dropdown-content 
+        {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    
+<div class="upper">
+    <div class="center" style="float:left;">
+        <a href="admindashboard.php"  ><img src="image/foot.png" alt="Shop Logo"></a>
+    </div>
+    
+    <ul >
+        <li  style="float: right;"><a href="#"  class="center" > Login</a></li>
+        <li class="dropdown" style="float: right;"><a href="#" class="dropbtn">Menu</a>
+            <div class="dropdown-content">
+                <a href="#">Manage Category</a>
+                <a href="#">Manage Product</a>
+                <a href="#">Manage Order</a>
+                <a href="#">Manage Customer</a>
+                <a href="#">Manage Staff </a>
+                <a href="#">View n Print Report </a>
+            </div>
+            </li>
+    </ul>
+
+
+
+
+        </div>
+
+    
+</body>
+</html>
