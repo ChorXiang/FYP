e590a1c8879a2789ee100bf611c93a0f5d58ce15
Murphy1211201894 <1211201894@student.mmu.edu.my>
2023-04-12 17:33:14 +0800

    header

--- adminheader.php
+++ adminheader.php
@@ -151,13 +151,14 @@
 
         
 <div class="sidenav">
-        <a href="admindashboard.php" class="center" ><img src="image/foot.png" alt="Shop Logo"></a><br><br> 
+        <a href="#" class="center" ><img src="image/foot.png" alt="Shop Logo"></a><br><br> 
         <a href="#">Manage Category</a>
         <a href="admin_product.php">Manage Product</a>
         <a href="admin_history.php">Manage Order</a>
-        <a href="manageuser.php">Manage Customer</a>
-        <a href="#">Manage Staff </a>
+        <a href="adminhomepage.php">Manage Customer</a>
+        <a href="managestaff.php">Manage Staff </a>
         <a href="managecomment.php">Manage comment </a>
+        <a href="managecontactus.php">Manage Support </a>
 </div>
 
 
