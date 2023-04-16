58183448f0bd7320ad599c4e00dd922b14514abe
Murphy1211201894 <1211201894@student.mmu.edu.my>
2023-04-16 14:56:01 +0800

    admin all shoes page

--- admin_shoes.php
+++ admin_shoes.php
@@ -20,7 +20,7 @@
     float:right;
     padding-right:80px ;
   }
-  .container
+  /* .container
   {
   display: flex;
   justify-content: space-between;
@@ -28,14 +28,16 @@
   margin: 0 auto;
   border: 1px solid #ccc;
   padding: 20px;
-  }
+  } */
 
   .imgcenter {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-  }
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 20%;
+      padding-left: 20px;
+      text-align:"center";  
+    }
 
   .wordcenter {
   display: block;
@@ -55,38 +57,46 @@
 
 <div class="container">
 
-<div class='right'>
-<fieldset>
+<div>
 
-<table>
-  <thead>
-    <tr>
-      <th>Shoe ID</th>
-      <th>Shoe Name</th>
-      <th>Shoe Image</th>
-      <th>Shoe Price</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($rows as $row): ?>
+<fieldset>
+    <?php
+        $sql = "select * from shoes";
+        $result = mysqli_query($conn,$sql);
+        ?>
+      <table border="0px">
+      <thead>
+        <tr>
+        <th>Shoe ID</th>
+        <th>Shoe Name</th>
+        <th>Shoe Image</th>
+        <th>Shoe Price</th>
+        </tr>
+    </thead>
+        <?php
+
+        while($row = mysqli_fetch_array($result))
+        {
+            ?>
+      <tbody>
       <tr>
         <td><?php echo $row['shoe_id']; ?></td>
         <td><?php echo $row['shoe_name']; ?></td>
-        <td><img src="<?php echo $row['shoe_image']; ?>" alt="<?php echo $row['shoe_name']; ?>"></td>
+        <td><img src="image/shoesimg/<?php echo $row["shoe_image"]; ?>" alt="<?php echo $row["shoe_name"]; ?>" class="imgcenter" ></td>
         <td><?php echo $row['shoe_price']; ?></td>
       </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+    </tbody>
+            <?php
 
+        }
+
+		?>
+			
+      </table>
 </fieldset>
+
 </div>
 </div>
 
-<?php echo "<div>".$msg."</div>"?>
 </body>
 </html>
-
-<?php
-    include 'footer.php';
-?>
