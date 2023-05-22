52e7ba99a3fa7c0398e86f3668d5f4ab5272d40c
ChorXiang <1211201763@student.mmu.edu.my>
2023-05-23 00:37:12 +0800

    link ok

--- product.php
+++ product.php
@@ -287,16 +287,17 @@ if (isset($_SESSION['user_id'])) {
  <?php if ($totalPages > 1): ?>
     <div class="pagination">
       
-      <?php if ($currentPage > 1): ?>
-        <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $currentPage - 1; ?>">Previous</a>
-      <?php endif; ?>
+    <?php if ($currentPage > 1): ?>
+    <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $currentPage - 1; ?>&user_id=<?php echo $id; ?>">Previous</a>
+<?php endif; ?>
+
       
       <?php for ($i = 1; $i <= $totalPages; $i++): ?>
-        <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $i; ?>" <?php if ($i == $currentPage) echo 'class="active"'; ?>><?php echo $i; ?></a>
+        <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $i; ?>&user_id=<?php echo $id; ?>" <?php if ($i == $currentPage) echo 'class="active"'; ?>><?php echo $i; ?></a>
       <?php endfor; ?>
 
       <?php if ($currentPage < $totalPages): ?>
-        <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $currentPage + 1; ?>">Next</a>
+        <a href="?search=<?php echo isset($_GET['search']) ? $_GET['search'] : ''; ?>&page=<?php echo $currentPage + 1; ?>&user_id=<?php echo $id; ?>">Next</a>
       <?php endif; ?>
       
     </div>
