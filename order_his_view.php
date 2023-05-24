560a2138fd07ca85c5ddec07821561371aa6f2ef
Murphy1211201894 <1211201894@student.mmu.edu.my>
2023-05-24 18:18:27 +0800

    layout

--- order_his_view.php
+++ order_his_view.php
@@ -26,62 +26,63 @@
             background-color: #f2f2f2;
             width: 100%;
             max-width: 1100px;
-        margin: auto; 
-        padding:50px;
+            margin: auto; 
+            padding:50px;
         }
         sup 
-    {
-     color: red;
-    }
-    body
-    {
-     background: #DDDDDD;
+        {
+        color: red;
+        }
+        body
+        {
+        background: #DDDDDD;
 
-    }
-    .box
-    {
-     background-color: #f2f2f2;
-     padding: 5px 20px 15px 20px;
-     border: 1px solid lightgrey;
-     border-radius: 15px;
-     width:50%;
-     margin: 20px 460px;
-    }
-    .box3 
-    {
-     width: 35%;
-     height: 100px;
-     margin:5px;
-     text-align:left;
-    }
-    .box5
-    {
-     width: 20%;
-     height: 100px;
-     text-align:left;
-    }
-    .box4
-    {
-     width: 15%;
-     height: 100px;
-     text-align:left;
-    }
-    .label
-    {
-     display: flex;
-     flex-wrap: wrap;
-     }
-     .label {
-  display: inline-block;
-  width: 150px;
-  font-weight: bold;
-  margin-bottom: 5px;
-}
+        }
+        .box
+        {
+        background-color: #f2f2f2;
+        padding: 5px 20px 15px 20px;
+        border: 1px solid lightgrey;
+        border-radius: 15px;
+        width:50%;
+        margin: 20px 460px;
+        text-align: left;
+        }
+        .box3 
+        {
+        width: 35%;
+        height: 100px;
+        margin:5px;
+        text-align:left;
+        }
+        .box5
+        {
+        width: 20%;
+        height: 100px;
+        text-align:left;
+        }
+        .box4
+        {
+        width: 15%;
+        height: 100px;
+        text-align:left;
+        }
+        .label
+        {
+        display: flex;
+        flex-wrap: wrap;
+        }
+        .label {
+        display: inline-block;
+        width: 150px;
+        font-weight: bold;
+        margin-bottom: 5px;
+        }
 
-.value {
-  display: inline-block;
-  margin-bottom: 5px;
-}
+        .value {
+        display: inline-block;
+        margin-bottom: 5px;
+        }
 
 
     </style>
@@ -89,11 +90,11 @@
 <body>
 
 <div class="middle">
-    <div class="table_center">
+    <div>
     <br><h1>VIEW ORDER DETAIL</h1><br>
     <div class="box">
-    <div class=".table_center">
-    <table class="table" border="1px">
+    <div>
+    <table border="1px">
         <?php
                     $id =$_GET['user_id'];
                     $order_num=$_GET['order_num'];
@@ -104,15 +105,15 @@
 
 ?>
 
-<br><label>Customer name :</label> <?php echo $row["his_name"];?>
+<br><label>Customer Name&nbsp;&nbsp;&nbsp; :</label> <?php echo $row["his_name"];?>
 
-<br><label>Customer phone number :</label> <?php echo $row["his_pn"];?>
+<br><label>Customer Contact :</label> <?php echo $row["his_pn"];?>
 
-<br><label>Customer address :</label> <?php echo $row["his_address"];?> <?php echo $row["his_state"];?>
+<br><label>Customer Address :</label> <?php echo $row["his_address"];?> <?php echo $row["his_state"];?>
 
-<br><label>Email :</label> <?php echo $row["her_email"];?>
+<br><label>Customer Email&nbsp;&nbsp;&nbsp; :</label> <?php echo $row["her_email"];?>
 
-<br><label>Date :</label> <?php echo $row["her_date"];?>
+<br><label>Purchase Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :</label> <?php echo $row["her_date"];?>
 
 
 
@@ -128,7 +129,6 @@
            
                 while($row = mysqli_fetch_assoc($result)) 
                 {                    
-                    $her_id = $row["her_id"];
                     $order_num = $row["order_num"];
                     $her_shoesname = $row["her_shoesname"];
                     $her_size = $row["her_size"];
@@ -141,20 +141,18 @@
                     $status = $row["order_status"];    
                     $her_date = $row["her_date"];             
                     ?>
-                    <br>
-                    <label>Shoes Iamge :</label> <img class='img' src="<?php echo "image/shoesimg/".$row['shoe_image'];?>" >
+                    <br><br>
+                     <img class='img' src="<?php echo "image/shoesimg/".$row['shoe_image'];?>" >
 
-                    <br><label>Order ID :</label> <?php echo $row["her_id"]; ?>
+                    <br><label>Shoes Name&emsp;&emsp; :</label> <?php echo $row["her_shoesname"];	?>
 
-                    <br><label>Shoes Name :</label> <?php echo $row["her_shoesname"];	?>
+                    <br><label>Shoes Size&emsp;&emsp;&emsp;: UK</label> <?php echo $row["her_size"];	?>
 
-                    <br><label>Shoes Size : UK</label> <?php echo $row["her_size"];	?>
+                    <br><label>Shoes Quantity&emsp; :</label> <?php echo $row["her_quantity"];?>
 
-                    <br><label>Quantity :</label> <?php echo $row["her_quantity"];?>
+                    <br><label>Price&emsp;&emsp;&emsp;&emsp;&emsp; : RM </label> <?php echo $row["her_price"];?>
 
-                    <br><label>Price : RM </label> <?php echo $row["her_price"];?>
-
-                    <br><label>Total Price : RM </label> <?php echo $total;?>
+                    <br><label>Total Price&emsp;&emsp;&emsp;: RM </label> <?php echo $total;?>
 
                         </tbody>
     
